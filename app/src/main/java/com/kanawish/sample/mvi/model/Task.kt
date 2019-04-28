9783f1805bfa94ee933041ead26b469b64f6de47
skustra <kustraslawomir@gmail.com>
2019-04-28 11:31:10 +0200

    init

---
+++ app/src/main/java/com/kanawish/sample/mvi/model/Task.kt
@@ -0,0 +1,11 @@
+package com.kanawish.sample.mvi.model
+
+import java.util.*
+
+data class Task(
+        val id  : String = UUID.randomUUID().toString(),
+        val lastUpdate : Long,
+        val title : String,
+        val description : String,
+        val completed : Boolean
+)
